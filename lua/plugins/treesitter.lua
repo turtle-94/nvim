13f27aface4ffca013487715198580d6ec71ca2e
mick <mick@highbiza.nl>
2025-04-11 17:38:11 +0200

    add treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,37 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function () 
-      local configs = require("nvim-treesitter.configs")
+        local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "css", "jinja", "yaml"},
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
+        configs.setup({
+            ensure_installed = {
+                "c",
+                "lua",
+                "vim", 
+                "vimdoc",
+                "query",
+                "elixir",
+                "heex",
+                "javascript",
+                "html", 
+                "python",
+                "css",
+                "jinja",
+                "yaml"
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
+            incremental_selection = {
+                enable = true,
+--                keymaps = {
+--                    init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
+--                    node_incremental = "<leader>si",
+--                    scope_incremental = "<leader>sc",
+--                    node_decremental = "<leader>sd",
+--                },
+            },
         })
     end
 }
